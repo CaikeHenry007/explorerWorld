ee43c5916db1816074ea2d7f093153ce550cbfb6
kamile5907 <kamilesilva436@gmail.com>
2024-09-04 15:45:41 -0300

    ok

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,35 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { StatusBar } from "expo-status-bar";
 import {
-  View,
   Text,
+  View,
   ImageBackground,
   TextInput,
-  TouchableOpacity,
-  KeyboardAvoidingView,
-  Platform,
-  ScrollView,
   Keyboard,
   TouchableWithoutFeedback,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+  Animated,
 } from "react-native";
-import { initializeApp } from "@firebase/app";
+import Svg, { Path } from "react-native-svg";
+import { initializeApp } from "@firebase/app"; // Corrigido o import do Firebase
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   onAuthStateChanged,
   signOut,
-} from "@firebase/auth";
+} from "@firebase/auth"; // Corrigido o import do Firebase
 import { useNavigation } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { StatusBar } from "expo-status-bar";
 import stylesInicio from "../styles/StyleInicio";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+const windowHeight = Dimensions.get("window").height / 2;
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyA51Q_cRKLbEJR2yohL_EQ_9eITyJNsbc0",
   authDomain: "explorer-word-827f9.firebaseapp.com",
   projectId: "explorer-word-827f9",
-  storageBucket: "explorer-word-827f9.appspot.com",
+  storageBucket: "explorer-word-827f9",
   messagingSenderId: "768160576189",
   appId: "1:768160576189:web:7b8e36f0ac872da07dfdd4",
   measurementId: "G-XW8Q60N51C",
@@ -72,14 +76,14 @@ export default function Inicio() {
       alert("Erro na autenticação!");
     }
   };
-
+  
   useEffect(() => {
     if (user) {
       navigation.navigate("Home", { handleAuthentication });
     }
   }, [user, navigation]);
 
-  const [font] = useFonts({
+  const [fontLoaded] = useFonts({
     Pacifico: require("../fonts/Pacifico-Regular.ttf"),
     Bebas: require("../fonts/Bebas.ttf"),
     Noto: require("../fonts/NotoSherif.ttf"),
@@ -87,100 +91,154 @@ export default function Inicio() {
     BonaNovaBold: require("../fonts/BonaNovaBold.ttf"),
   });
 
-  if (!font) {
+  if (!fontLoaded) {
     return null;
   }
 
+  const heightAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    // Animação para aumentar a altura da View
+    Animated.timing(heightAnim, {
+      toValue: windowHeight, // Alvo da altura final
+      duration: 1500, // Duração da animação em milissegundos
+      useNativeDriver: false, // Não é possível usar o native driver para height
+    }).start();
+  }, [heightAnim]);
+
   return (
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-    >
+    <View style={{ flex: 1 }}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={stylesInicio.container}>
-          <StatusBar backgroundColor="#000000" color="#fff" />
+        <View style={{ flex: 1 }}>
+          <StatusBar style="light" />
           <ImageBackground
             style={stylesInicio.imgBackground}
             resizeMode="cover"
             source={require("../images/Home.jpg")}
+            blurRadius={10}
           >
             {user ? (
               <Text>Logado!</Text>
             ) : (
-              <View style={stylesInicio.card}>
-                <ScrollView
-                  contentContainerStyle={stylesInicio.scrollContainer}
-                  keyboardShouldPersistTaps="handled"
+              <View>
+                <View
+                  style={{
+                    width: "100%",
+                    height: "40%",
+                    paddingLeft: 30,
+                    justifyContent: "center",
+                  }}
                 >
-                  <Text style={stylesInicio.titulo}>
-                    {isLogin ? "Login" : "Cadastro"}
-                  </Text>
-                  <View style={{ height: "65%" }}>
-                    <View style={stylesInicio.viewcaixa}>
-                      <MaterialCommunityIcons
-                        name="email"
-                        size={20}
-                        color={"#c0c0c0"}
-                      />
-                      <TextInput
-                        style={stylesInicio.caixaTexto}
-                        value={email}
-                        onChangeText={setEmail}
-                        placeholder="Email"
-                        autoCapitalize="none"
-                      />
-                    </View>
+                  <Image
+                    source={require("../images/logobranco.png")}
+                    style={{
+                      width: "80%",
+                      height: "40%",
+                    }}
+                  />
+                </View>
 
-                    <View style={stylesInicio.viewcaixa}>
-                      <MaterialCommunityIcons
-                        name="lock"
-                        size={20}
-                        color={"#c0c0c0"}
-                      />
-                      <TextInput
-                        style={stylesInicio.caixaTexto}
-                        value={password}
-                        onChangeText={setPassword}
-                        placeholder="Senha"
-                        secureTextEntry
-                      />
-                    </View>
+                <Animated.View
+                  style={{
+                    position: "absolute",
+                    bottom: 0,
+                    width: "100%",
+                    height: heightAnim,
+                  }}
+                >
+                  <Svg
+                    viewBox="0 0 1440 320"
+                    width="100%"
+                    height="20%"
+                    preserveAspectRatio="none"
+                    style={{ marginBottom: -1 }}
+                  >
+                    <Path
+                      fill="#ECD29C"
+                      fillOpacity="1"
+                      d="M0,128L60,112C120,96,240,64,360,85.3C480,107,600,181,720,213.3C840,245,960,235,1080,240C1200,245,1320,267,1380,277.3L1440,288L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+                    />
+                  </Svg>
 
-                    {isLogin && (
-                      <TouchableOpacity
-                        onPress={() => navigation.navigate("RecuperarSenha")}
-                      >
-                        <Text style={stylesInicio.recuperarText}>
-                          Esqueceu sua senha?
+                  <View
+                    style={{
+                      flex: 1,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      backgroundColor: "#ECD29C",
+                    }}
+                  >
+                    <Text style={stylesInicio.titulo}>
+                      {isLogin ? "Login" : "Cadastro"}
+                    </Text>
+                    <View>
+                      <View >
+                        <MaterialCommunityIcons
+                          name="email"
+                          size={20}
+                          color={"#c0c0c0"}
+                        />
+                        <TextInput
+                          style={stylesInicio.caixaTexto}
+                          value={email}
+                          onChangeText={setEmail}
+                          placeholder="Email"
+                          autoCapitalize="none"
+                        />
+                      </View>
+
+                      <View >
+                        <MaterialCommunityIcons
+                          name="lock"
+                          size={20}
+                          color={"#c0c0c0"}
+                        />
+                        <TextInput
+                          style={stylesInicio.caixaTexto}
+                          value={password}
+                          onChangeText={setPassword}
+                          placeholder="Senha"
+                          secureTextEntry
+                        />
+                      </View>
+
+                      {isLogin && (
+                        <TouchableOpacity
+                          onPress={() => navigation.navigate("RecuperarSenha")}
+                        >
+                          <Text style={stylesInicio.recuperarText}>
+                            Esqueceu sua senha?
+                          </Text>
+                        </TouchableOpacity>
+                      )}
+
+                      <View style={stylesInicio.viewbotoes}>
+                        <TouchableOpacity
+                          style={stylesInicio.btn}
+                          onPress={handleAuthentication}
+                        >
+                          <Text style={stylesInicio.btntxt}>
+                            {isLogin ? "Login" : "Cadastre-se"}
+                          </Text>
+                        </TouchableOpacity>
+
+                        <Text
+                          onPress={() => setIsLogin(!isLogin)}
+                          style={stylesInicio.textoMenor}
+                        >
+                          {isLogin
+                            ? "Ainda não tem conta? Cadastre-se"
+                            : "Já tem uma conta? Login"}
                         </Text>
-                      </TouchableOpacity>
-                    )}
-
-                    <View style={stylesInicio.viewbotoes}>
-                      <TouchableOpacity
-                        style={stylesInicio.btn}
-                        onPress={handleAuthentication}
-                      >
-                        <Text style={stylesInicio.btntxt}>
-                          {isLogin ? "Login" : "Cadastre-se"}
-                        </Text>
-                      </TouchableOpacity>
-
-                      <Text
-                        onPress={() => setIsLogin(!isLogin)}
-                        style={stylesInicio.textoMenor}
-                      >
-                        {isLogin
-                          ? "Ainda não tem conta? Cadastre-se"
-                          : "Já tem uma conta? Login"}
-                      </Text>
+                      </View>
                     </View>
                   </View>
-                </ScrollView>
+                </Animated.View>
               </View>
             )}
           </ImageBackground>
         </View>
       </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+    </View>
   );
 }
